9619ea1d9e4c06b9c69f549c048739e4ac4fafb6
David Morgan <david.r.morgan.92@gmail.com>
2026-09-07 20:17:41 +0100

    use bootstrap modal for event instructions

--- static/plans/plan_calendar.js
+++ static/plans/plan_calendar.js
@@ -5,6 +5,8 @@
     const events = JSON.parse(
       document.getElementById(calendarElement.dataset.eventsId).textContent,
     );
+    const eventModalElement = calendarElement.parentElement.querySelector('[data-plan-event-modal]');
+    const eventInstructionsElement = eventModalElement.querySelector('[data-plan-event-instructions]');
     const calendar = new FullCalendar.Calendar(calendarElement, {
       initialView: 'dayGridMonth',
       firstDay: 1,
@@ -17,7 +19,11 @@
       events,
       eventClick: (info) => {
         const instructions = info.event.extendedProps.instructions;
-        if (instructions) window.alert(instructions);
+        if (!instructions) return;
+
+        eventInstructionsElement.textContent = instructions;
+        const eventModal = bootstrap.Modal.getOrCreateInstance(eventModalElement);
+        eventModal.show();
       },
     });
     calendarElement.dataset.initialized = 'true';
